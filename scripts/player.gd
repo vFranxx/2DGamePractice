f5b6d463858462e9eba3fc107560f3ca5de71aba
Fran <franciscoapighin@gmail.com>
2024-06-03 23:54:53 -0300

    New feature added
    Added CoyoteTime and missing jump sound effect

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,22 +6,41 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_jump = false
 
 @onready var animated_sprite = $AnimatedSprite2D
-
+@onready var coyote_timer = $CoyoteTimer
+@onready var jump_sound = $JumpSound
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	apply_gravity(delta)
+	handle_jump()
 	# Get the input direction {-1; 0; 1} and handle the movement/deceleration.
 	var direction = Input.get_axis("Move_left", "Move_right")
-	
+	handle_player_visuals(direction)
+	handle_player_movement(direction)
+	coyote_jump()
+	move_and_slide()
+
+func apply_gravity(delta):
+	# Apply gravity.
+	if !is_on_floor():
+		velocity.y += gravity * delta
+
+func handle_player_movement(direction):
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func handle_jump():
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") && (is_on_floor() || can_jump):
+		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+		can_jump = false
+
+func handle_player_visuals(direction):
 	#Flip the sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -30,16 +49,15 @@ func _physics_process(delta):
 	
 	#Play animations
 	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("animation_idle")
-		else: 
-			animated_sprite.play("animation_run")
+		animated_sprite.play("animation_idle" if direction == 0 else ("animation_run"))
 	else: 
 		animated_sprite.play("animation_jump")
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func coyote_jump():
+	var was_on_floor = is_on_floor()
+	
+	if was_on_floor:
+		coyote_timer.start()
+		can_jump = true
+	elif coyote_timer.is_stopped():
+		can_jump = false
